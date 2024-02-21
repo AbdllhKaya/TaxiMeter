83f736f00fdfee47b7ddc061ff28171f07f5a08e
Abdullah KAYA <abdullahky0409@gmail.com>
2024-02-21 19:40:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        //Scanner class has been defineted.
+        Scanner in= new Scanner(System.in);
+
+
+        //The variable data types have been defined.
+        double startPrice=10,total,perKM=2.20,km ;
+
+        //Get values from the user.
+        System.out.print("Please enter kilometer=");
+        km= in.nextDouble();
+
+        //The taxi price has been calculated.
+        total=perKM*km;
+        total+=startPrice;
+
+        total= (total>20) ? total : 20 ;
+
+        System.out.print("Taksi price=" + total);
+
+
+    }
+}
